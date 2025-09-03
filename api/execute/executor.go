f8ddd1b6b3ca6348433038046e5f81d4a510f1b1
Aromal <aromals161@gmail.com>
2025-09-03 22:35:20 +0530

    added ffi and used it in the golang tcp server

---
+++ api/execute/executor.go
@@ -0,0 +1,24 @@
+package executor
+
+/*
+#cgo LDFLAGS: -L../../ffi/target/x86_64-pc-windows-msvc/release -lengine
+#include <stdlib.h>
+
+extern char* execute_sql(const char* sql);
+extern void free_string(char* ptr);
+*/
+import "C"
+import "unsafe"
+
+func Execute(query string) string {
+    cQuery := C.CString(query)
+    defer C.free(unsafe.Pointer(cQuery))
+
+    res := C.execute_sql(cQuery)
+    if res == nil {
+        return "Error: Rust returned null"
+    }
+    defer C.free_string(res)
+
+    return C.GoString(res)
+}
---
+++ ffi/target/release/deps/engine.d
@@ -0,0 +1,7 @@
+D:\personal-projects\SynapseDB\ffi\target\release\deps\engine.d: src\lib.rs
+
+D:\personal-projects\SynapseDB\ffi\target\release\deps\libengine.rlib: src\lib.rs
+
+D:\personal-projects\SynapseDB\ffi\target\release\deps\engine.dll: src\lib.rs
+
+src\lib.rs:
