d150cf6b5902fee39bea9660d030cd356574c673
Aromal <aromals161@gmail.com>
2025-09-03 17:26:49 +0530

    tcp server built

---
+++ server/tcp_server.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+
+	ln, err := net.Listen("tcp", ":4538")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for {
+		conn, err := ln.Accept()
+
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		go handleConnection(conn)
+	}
+
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	buf := make([]byte, 1024)
+	_, err := conn.Read(buf)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("Received: %s", buf)
+}
