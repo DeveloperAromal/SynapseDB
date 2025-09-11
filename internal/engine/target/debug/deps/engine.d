7ce5dbe9fe9d4711ed13c8bbe26af3d5c3ef5e6d
Aromal <aromals161@gmail.com>
2025-09-11 21:38:45 +0530

    fixed prompt

---
+++ internal/engine/target/debug/deps/engine.d
@@ -0,0 +1,15 @@
+D:\personal-projects\SynapseDB\engine\target\debug\deps\engine.d: src\main.rs src\query_processor\mod.rs src\query_processor\ast.rs src\query_processor\tokenizer.rs src\query_processor\parser.rs src\query_processor\executor.rs src\storage\mod.rs src\storage\page.rs src\storage\row.rs src\storage\table.rs src\storage\disk.rs
+
+D:\personal-projects\SynapseDB\engine\target\debug\deps\engine.exe: src\main.rs src\query_processor\mod.rs src\query_processor\ast.rs src\query_processor\tokenizer.rs src\query_processor\parser.rs src\query_processor\executor.rs src\storage\mod.rs src\storage\page.rs src\storage\row.rs src\storage\table.rs src\storage\disk.rs
+
+src\main.rs:
+src\query_processor\mod.rs:
+src\query_processor\ast.rs:
+src\query_processor\tokenizer.rs:
+src\query_processor\parser.rs:
+src\query_processor\executor.rs:
+src\storage\mod.rs:
+src\storage\page.rs:
+src\storage\row.rs:
+src\storage\table.rs:
+src\storage\disk.rs:
