f8d5afa7aa35cfc41945344d89282efba4051f64
Aromal S <153208245+DeveloperAromal@users.noreply.github.com>
2025-10-27 05:37:25 +0530

    Delete ffi/target directory

--- ffi/target/release/deps/engine.d
+++
@@ -1,7 +0,0 @@
-D:\personal-projects\SynapseDB\ffi\target\release\deps\engine.d: src\lib.rs
-
-D:\personal-projects\SynapseDB\ffi\target\release\deps\libengine.rlib: src\lib.rs
-
-D:\personal-projects\SynapseDB\ffi\target\release\deps\engine.dll: src\lib.rs
-
-src\lib.rs:
