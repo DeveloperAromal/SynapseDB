a9c81e74212ca250fc141bba300eef28d5e07cab
Aromal S <153208245+DeveloperAromal@users.noreply.github.com>
2025-11-05 18:42:39 +0530

    Delete ffi/target directory

--- ffi/target/debug/deps/engine-9290449e2017cb42.d
+++
@@ -1,5 +0,0 @@
-D:\personal-projects\SynapseDB\ffi\target\debug\deps\engine-9290449e2017cb42.d: src\lib.rs
-
-D:\personal-projects\SynapseDB\ffi\target\debug\deps\libengine-9290449e2017cb42.rmeta: src\lib.rs
-
-src\lib.rs:
