9ac5d90e5d06fb45b9818541709a61968332ee18
DeveloperAromal <developeraromal@gmail.com>
2025-12-16 16:49:01 +0530

    Created a parser that parses connection url

---
+++ server/connecton/parser.go
@@ -0,0 +1,42 @@
+package connecton
+
+import (
+	"errors"
+	"net/url"
+	"strings"
+)
+
+type ConnInfo struct {
+						Username string
+						Password string
+						Host     string
+						DBName   string
+						SSL      string
+					}
+
+func parseUrl(connStr string) (*ConnInfo, error) {
+
+	u, err := url.Parse(connStr)
+
+	if err != nil {
+		return nil, errors.New("Invalid Connection String")
+	}
+
+	if u.User != nil {
+		return nil, errors.New("Missing Credentials")
+
+	}
+	username := u.User.Username()
+	password, _ := u.User.Password()
+	host := u.Host
+	dbName := strings.TrimPrefix(u.Path, "/")
+	ssl := u.Query().Get("ssl")
+
+	return &ConnInfo{
+						Username: username,
+						Password: password,
+						Host:     host,
+						DBName:   dbName,
+						SSL:      ssl,
+					}, nil
+}
